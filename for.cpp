de3d9764caeee92cf025fff07ac7c17d7e578ff1
ashi-ish <parulekar.ashish17@gmail.com>
2025-07-31 22:50:53 -0400

    for, while, do while loops start

---
+++ for.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int i, n;
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Value of i: " << i << endl;
+    }
+    return 0;
+}
---
+++ while.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // int i, n;
+    // cin >> n;
+    // for (i = 0; i < n; i++)
+    // {
+    //     cout << "Value of i: " << i << endl;
+    // }
+    // int i = 0, n;
+    // cin >> n;
+    // while (i < n)
+    // {
+    //     cout << "Value of i: " << i << endl;
+    //     i++;
+    // }
+    // return 0;
+    int i = 0, n;
+    cin >> n;
+    do
+    {
+        cout << "Value of i: " << i << endl;
+    } while (i < n);
+    return 0;
+}
