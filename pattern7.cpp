f7eaa794a6bbceb4a4bfb475eba594d7e3870ae4
ashi-ish <parulekar.ashish17@gmail.com>
2025-08-14 10:05:13 -0400

    pattern programming, pattern 7

--- pattern7.cpp
+++ pattern7.cpp
@@ -4,22 +4,30 @@
 #include <string>
 using namespace std;
 
-void pattern6(int n)
+// ************* [0, 13, 0]
+//  ***********  [1, 11, 1]
+//   *********  [2, 9, 2]
+//    ******* [3, 7, 3]
+//     ***** [4, 5, 4]
+//      *** [5, 3, 5]
+//       *   [6, 1, 6]
+
+void pattern7(int n)
 {
     for (int i = 0; i < n; i++)
     {
         // space
-        for (int j = 1; j <= n - i - 1; j++)
+        for (int j = 1; j <= i; j++)
         {
             cout << " ";
         }
         // stars
-        for (int j = 1; j <= 2 * i + 1; j++)
+        for (int j = 1; j <= (2 * n - (2 * i + 1)); j++)
         {
             cout << "*";
         }
         // space
-        for (int j = 1; j <= n - i - 1; j++)
+        for (int j = 1; j <= i; j++)
         {
             cout << " ";
         }
@@ -34,7 +42,7 @@ int main()
     for (int i = 0; i < t; i++) // checking for different test cases
     {
         cin >> n;
-        pattern6(n);
+        pattern7(n);
     }
 
     return 0;
