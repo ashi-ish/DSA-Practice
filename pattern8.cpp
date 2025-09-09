1baa080ac5b20ffffce70ceef25a5d99fbe357e8
ashi-ish <parulekar.ashish17@gmail.com>
2025-09-08 22:36:36 -0400

    pattern8.cpp implementation

---
+++ pattern8.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void pattern6(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 1; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // stars
+        int breakpoint = (2 * i + 1) / 2;
+        char ch = 'A';
+        for (int j = 1; j <= 2 * i + 1; j++)
+        {
+            if(j <= breakpoint) cout << ch++;
+            else cout << ch--;
+        }
+        // space
+        for (int j = 1; j <= n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n, t;
+    cin >> t;
+    for (int i = 0; i < t; i++) // checking for different test cases
+    {
+        cin >> n;
+        pattern6(n);
+    }
+
+    return 0;
+}
